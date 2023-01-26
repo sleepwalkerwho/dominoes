7cc1950cce78fb9720ad3320c6931b706d8f7ab5
sleepwalkerwho <58708251+sleepwalkerwho@users.noreply.github.com>
2023-01-26 22:23:34 +0300

    feat: add rules

--- dominoes.py
+++ dominoes.py
@@ -40,75 +40,73 @@ def find_snake(computer, player):
     return player_turn, domino
 
 
-def print_invalid_input(stock, domino, computer, player):
-    print("Invalid input. Please try again.")
-    print_all(stock, domino, computer, player)
-
-
-def player_move(domino, computer, player, stock):
-    flag = True
-    move = input()
-    # 0 -incorrenct, 1 - plus, 2 - minus, 3 - 0, 4 - comp
-    result = check_input(move)
-    if result == 0:
-        print_invalid_input(stock, domino, computer, player)
-        print("\nStatus: It's your turn to make a move. Enter your command.")
-        domino, computer, player, stock, flag = player_move(domino, computer, player, stock)
-    elif result == 1:
-        move = int(move)
-        if move <= len(player):
-            domino.append(player[move - 1])
-            player.remove(player[move - 1])
-        else:
-            print_invalid_input(stock, domino, computer, player)
-            print("\nStatus: It's your turn to make a move. Enter your command.")
-            domino, computer, player, stock, flag = player_move(domino, computer, player, stock)
-    elif result == 2:
-        move = -int(move)
-        if move <= len(player):
-            domino.insert(0, player[move - 1])
-            player.remove(player[move - 1])
-        else:
+def player_input_check(stock, domino, computer, player, next_player):
+    p_move = ""
+    ok = True
+    while ok:
+        p_move = input()
+        try:
+            player[abs(int(p_move)) - 1]
+            p_move = int(p_move)
+            ok = False
+        except:
             print("Invalid input. Please try again.")
-            print_all(stock, domino, computer, player)
-            print("\nStatus: It's your turn to make a move. Enter your command.")
-            domino, computer, player, stock, flag = player_move(domino, computer, player, stock)
-    elif result == 3:
-        if len(stock) > 0:
-            stock, player = take_from_stock(stock, player)
-        elif len(stock) <= 0:
-            flag = check_win(computer, player, domino, stock)
+            print_all(stock, domino, computer, player, next_player)
+    return p_move
 
-    elif result == 4:
-        flag = True
-        if len(computer) > 0:
-            move = random.randint(0, len(computer) - 1)
-            domino.append(computer[move])
-            computer.remove(computer[move])
-        if len(computer) == 0:
-            flag = check_win(computer, player, domino, stock)
+
+def check_negative(number, player):
+    if number > 0:
+        return False
+    elif number < 0:
+        number = -number
+        if number > len(player):
+            return True
+        elif number <= len(player):
+            return False
+
+def check_player_move(domino, move, player, stock, computer, next_player):
+    move_list = player[move-1]
+    if domino[-1][-1] == move_list[0]:
+        return move_list, player.index(move_list), True
+    elif domino[-1][-1] == move_list[-1]:
+        return move_list.reverse(), player.index(move_list), True
+    else:
+        print("Illegal move. Please try again.")
+        #print_all(stock,domino,computer,player,next_player)
+        return move_list, player.index(move_list), False
+
+
+def player_move(stock, domino, computer, player, next_player):
+    move = player_input_check(stock, domino, computer, player, next_player)
+    if move < 0 and -move > len(player):
+        return 1
+    elif move > 0 and move <= len(player):
+        move_list, index, is_ok = check_player_move(domino, move, player, stock, computer, next_player)
+        if is_ok is False:
+            move = player_input_check(stock, domino, computer, player, next_player)
+        else:
+            domino.append(player[index])
+            player.remove(player[index])
+    elif move < 0 and (len(player) + move > 0):
+        move = -move
+        domino.insert(0, player[move - 1])
+        player.remove(player[move - 1])
+    elif move == 0 and len(stock) > 0:
+            stock, player = take_from_stock(stock, player)
     return domino, computer, player, stock, flag
 
 
-def check_input(move):
-    '''try:
-        move = int(move)
-    except ValueError or IndexError:
-        print("Invalid input. Please try again.")'''
-    result = 0
-    if move.isdigit():
-        move = int(move)
-        if move > 0:
-            result = 1
-        elif move == 0:
-            result = 3
-        else:
-            result = 2
-    elif move.isalpha():
-        result = 0
-    elif move == "" and len(move) == 0:
-        result = 4
-    return result
+def computer_move(computer, player, domino, stock):
+    move = input()
+    flag = True
+    if len(computer) > 0:
+        move = random.randint(0, len(computer) - 1)
+        domino.append(computer[move])
+        computer.remove(computer[move])
+    if len(computer) == 0:
+        flag = check_win(computer, player, domino, stock)
+    return computer, player, domino, stock, flag
 
 
 def take_from_stock(stock, pieces):
@@ -126,7 +124,7 @@ def check_win(computer, player, snake, stock):
         return flag
     elif draw == 0:
         if len(computer) == 0:
-            print_all(stock, snake, computer, player)
+            print_all(stock, snake, computer, player, next_player)
             print("\nSStatus: The game is over. The computer won!")
             flag = False
         elif len(player) == 0:
@@ -150,7 +148,7 @@ def is_draw(snake):
         return 0
 
 
-def print_all(stock, domino, computer, player):
+def print_all(stock, domino, computer, player, next_player):
     print("======================================================================")
     print(f"Stock size: {len(stock)}")
     print(f"Computer pieces: {len(computer)}")
@@ -165,6 +163,10 @@ def print_all(stock, domino, computer, player):
         for i in player:
             print(f"{player_count}:{i}")
             player_count += 1
+    if next_player == 'player':
+        print("\nStatus: It's your turn to make a move. Enter your command.")
+    elif next_player == 'computer':
+        print("\nStatus: Computer is about to make a move. Press Enter to continue...")
 
 
 def print_snake(domino_snake):
@@ -186,21 +188,15 @@ def print_snake(domino_snake):
 
 stock_pieces, computer_pieces, player_pieces = generate_stock_computer_player()
 next_player, domino_snake = find_snake(computer_pieces, player_pieces)
-
-snake = ""
 flag = True
 while flag:
-    print_all(stock_pieces, domino_snake, computer_pieces, player_pieces)
+    print_all(stock_pieces, domino_snake, computer_pieces, player_pieces, next_player)
     flag = check_win(computer_pieces, player_pieces, domino_snake, domino_snake)
     if flag == False:
         break
     if next_player == 'computer':
-        print("\nStatus: Computer is about to make a move. Press Enter to continue...")
-        domino_snake, computer_pieces, player_pieces, stock_pieces, flag = player_move(domino_snake, computer_pieces,
-                                                                                       player_pieces, stock_pieces)
+        computer_pieces, player_pieces, domino_snake, stock_pieces, flag = computer_move(computer_pieces, player_pieces, domino_snake, stock_pieces)
         next_player = "player"
     else:
-        print("\nStatus: It's your turn to make a move. Enter your command.")
-        domino_snake, computer_pieces, player_pieces, stock_pieces, flag = player_move(domino_snake, computer_pieces,
-                                                                                       player_pieces, stock_pieces)
+        domino_snake, computer_pieces, player_pieces, stock_pieces, flag = player_move(stock_pieces, domino_snake, computer_pieces, player_pieces, next_player)
         next_player = "computer"
